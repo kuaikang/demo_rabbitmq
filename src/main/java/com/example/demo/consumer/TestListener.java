16ef7c242bc2bbd11b053f9f9f4cd85eec9b7648
kuaik <359405466@qq.com>
2018-09-30 15:12:14 +0800

    ack 手动确认

--- src/main/java/com/example/demo/consumer/TestListener.java
+++ src/main/java/com/example/demo/consumer/TestListener.java
@@ -1,7 +1,10 @@
 package com.example.demo.consumer;
 
+import com.rabbitmq.client.Channel;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.AmqpHeaders;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 
 import java.text.SimpleDateFormat;
@@ -19,12 +22,19 @@ public class TestListener {
     SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
 
     @RabbitHandler
-    public void handler(String message) {
+    public void handler(String message, Channel channel,@Header(AmqpHeaders.DELIVERY_TAG) long tag) {
         System.out.println();
         for (int i = 0; i < 1000; i++) {
             System.out.print("1");
         }
         System.out.println();
         System.out.println("消费者------------->内容：" + message + ",时间：" + simpleDateFormat.format(new Date()));
+
+        try {
+            /* 手动确认*/
+            channel.basicAck(tag,false);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
